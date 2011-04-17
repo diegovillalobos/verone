6434d053acea7029bfcc3e641fa54b857f33c14f
DiegoVillalobos <d@dsblox.com>
2011-04-17 02:47:00 -0500

    searchbuton

--- wordpress/wp-content/themes/twentyten/sidebar.php
+++ wordpress/wp-content/themes/twentyten/sidebar.php
@@ -1,12 +1,4 @@
-<?php
-/**
- * The Sidebar containing the primary and secondary widget areas.
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
- */
-?>
+
 
 		<div id="primary" class="widget-area" role="complementary">
 			<ul class="xoxo">
