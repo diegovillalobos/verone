ce81f13d31bc5d32ede94a213a24a24a101fa2c0
DiegoVillalobos <d@dsblox.com>
2011-04-17 00:39:53 -0500

    site description on index tab title

--- wordpress/wp-content/themes/verone/header.php
+++ wordpress/wp-content/themes/verone/header.php
@@ -14,15 +14,15 @@
    global $page, $paged;
 
 
-	wp_title( '|', true, 'right' );
+	wp_title( ':' ,true, 'right' );
 	
 // Add the blog name.
 	bloginfo( 'name' );
 
 // Add the blog description for the home/front page.
-	get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() ) )
+	echo " : $site_description";
 
 ?></title>
 
