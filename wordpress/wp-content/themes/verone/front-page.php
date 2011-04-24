2e3dad6d65ad923afdd910857480f4cc8a6306ab
Santiago Villalobos <santivilla93@gmail.com>
2011-04-24 18:51:40 -0500

    Grodo fixed everything

---
+++ wordpress/wp-content/themes/verone/front-page.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+<section id="middle">
+	<div id="slideshow">
+		<img src="<?php echo esc_attr( get_bloginfo('template_directory', 'display')); ?>/images/header/mainpic.jpg" alt="Main Picture" width="917" height="272" />
+	</div>
+	<?php get_sidebar(); ?>
+	<div id="content">
+		<div id="home_columns">
+			<?php if ( ! dynamic_sidebar( 'home-content-widget-area' ) ) : ?>
+			<?php endif; ?>
+		</div>
+		
+		<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+		
+			<div id="post_<?php the_ID(); ?>" <?php post_class(); ?>>
+			
+				<div class="entry-image"> <?php echo wp_get_attachment_image( 1 ); ?> </div>
+				
+				
+				<div class="entry-content">
+					<?php the_content(__('Continue reading', 'example')); ?>
+					<?php wp_link_pages('before=<p class="pages">' . __('Pages:','example') . '&after=</p>'); ?>
+				</div>
+			
+			</div>
+		
+		<?php endwhile; ?>
+		
+		<?php else : ?>
+		
+			<p class="no-posts"><?php _e('Sorry, no posts matched your criteria', 'example'); ?></p>
+			
+		
+		<?php endif; ?>
+	
+	
+	</div>
+</section>
+<?php get_footer(); ?>
