6937e14a137f93798b6162715d0098a63f8c0072
DiegoVillalobos <d@dsblox.com>
2011-04-28 00:35:56 -0500

    styling comments layout

---
+++ wordpress/wp-content/themes/verone/comments.php
@@ -0,0 +1,4 @@
+<div id="comments">
+	<?php comment_form(); ?>
+	
+</div><!-- #comments -->
---
+++ wordpress/wp-content/themes/verone/single.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+	<div id="content">
+		<?php get_template_part( 'loop', 'single' );
+		?>
+	</div>
+	
+<?php get_footer();  ?>
