45961fad2b9acc09418df1bc31b03b3956f311d6
DiegoVillalobos <d@dsblox.com>
2011-05-01 09:31:57 -0500

    - Moved wp files to root of project

--- wp-content/themes/verone/single.php
+++ wp-content/themes/verone/single.php
@@ -2,9 +2,5 @@
 <section id="single">
 		<?php get_template_part( 'loop', 'single' );
 		?>
-<<<<<<< HEAD
-
-=======
->>>>>>> 1e5d1b3c160ca6789acee642cd83586c06d2cbfc
 </section>
 <?php get_footer();  ?>
