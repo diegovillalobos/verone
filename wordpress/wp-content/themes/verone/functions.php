37a3da601bfc77dc265009aea7d5e0806e35147f
Santiago Villalobos <santivilla93@gmail.com>
2011-05-01 08:08:25 -0500

    updated Ds files

--- wordpress/wp-content/themes/verone/functions.php
+++ wordpress/wp-content/themes/verone/functions.php
@@ -100,6 +100,7 @@ if ( ! isset( $content_width ) )
 
 
 function my_fields($fields) {
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 	$fields['subject'] = '<p class="comment-form-subject"><label for="subject">' . __( 'Subject' ) . '</label><input id="subject" name="subject" type="text" value="' . esc_attr( $commenter['comment_author_subject'] ) . '" size="30" /></p>';
 	
@@ -109,6 +110,9 @@ add_filter('comment_form_default_fields','my_fields');
 =======
 $fields['subject'] = '<p class="comment-form-subject"><label for="subject">' . ( 'Subject' ) . '</label>' .
 		            '<input id="subject" name="subject" type="text" value="' . esc_attr( $commenter['comment_author_subject'] ) . '" size="30" /></p>';
+=======
+	$fields['subject'] = '<p class="comment-form-subject"><label for="subject">' . __( 'Subject' ) . '</label><input id="subject" name="subject" type="text" value="' . esc_attr( $commenter['comment_author_subject'] ) . '" size="30" /></p>';
+>>>>>>> Stashed changes
 
 return $fields;
 }
