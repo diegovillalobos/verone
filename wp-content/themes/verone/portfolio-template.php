66693f15e035fefdd728c6237cf7514daa90e790
Santiago Villalobos <santivilla93@gmail.com>
2011-05-12 02:27:48 -0500

    Added the query commands for portfolio outputs

--- wp-content/themes/verone/portfolio-template.php
+++ wp-content/themes/verone/portfolio-template.php
@@ -7,27 +7,32 @@ Template Name: Portfolio
 <?php get_header(); ?>
 
 <section id="portfolio">
+
+		<?php
+				$query = new WP_Query( 'category_name=portfolio' );
+					while ( $query->have_posts() ) : $query->the_post();
+					echo '<h2 class="portfolio-title">';
+					the_title();
+					echo '</h2>';
+					echo '<p class="portfolio-main">' ;
+					the_content();
+					echo '</p>' ;
+						endwhile;
+					wp_reset_postdata();
+		?>		
+
 	<?php get_sidebar(); ?>
 
-
-	<div id="content">
-		<div id="portfolio_columns">
+	<div id="portfolio" class="portfolio_navigation" >
 			<?php if ( ! dynamic_sidebar( 'portfolio-content-widget-area' ) ) : ?>
 			<?php endif; ?>
-		</div>
 		
 		<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 		
 			<div id="post_<?php the_ID(); ?>" <?php post_class(); ?>>
 			
-				<div class="entry-image"> <?php echo wp_get_attachment_image( 1 ); ?> </div>
+				<div class="entry-image"> <?php echo wp_get_attachment_image( 1 ); ?> </div>		
 				
-				
-				<div class="entry-content">
-					<?php the_content(__('Continue reading', 'verone')); ?>
-					<?php wp_link_pages('before=<p class="pages">' . __('Pages:','example') . '&after=</p>'); ?>
-
-				</div>
 			
 			</div>
 		
